8d73c7cee602f4defdbfeeffe49e6b6b6e0f8b88
Rafael de Alvarenga Reis <rafa_alvarenga@msn.com>
2021-08-18 13:10:11 -0300

    Commit 18082020-1310

--- exercicios/Bloco-04/scripts.js
+++ exercicios/Bloco-04/scripts.js
@@ -1,5 +1,16 @@
-let age = 20;
-console.log(age)
+// let age = 20;
+// console.log(age)
 
-let anoAtual = new Date ().getFullYear();
-console.log(anoAtual);
+// let anoAtual = new Date ().getFullYear();
+// console.log(anoAtual);
+
+// let myName = 'Rafael';
+// console.log("Nome:"+ myName);
+
+// const birthCity = 'São Bernardo do Campo';
+// birthCity = 'São José dos Campos';
+// console.log('Cidade de Nascimento:' + birthCity);
+
+// let birthYear = 1989;
+// birthYear = 2030;
+// console.log('Ano de Nascimento:' + birthYear);
