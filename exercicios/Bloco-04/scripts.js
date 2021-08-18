8245780fbafeccc86b04bc532a89273e1197f235
Rafael de Alvarenga Reis <rafa_alvarenga@msn.com>
2021-08-18 18:11:37 -0300

    Commit 18082020-1811

--- exercicios/Bloco-04/scripts.js
+++ exercicios/Bloco-04/scripts.js
@@ -1,27 +1,24 @@
 // VARIÁVEIS
 
-// let age = 20;
-// console.log(age)
+let myName = 'Rafael de Alvarenga Reis';
+let anoAtual = new Date ().getFullYear();
+let anoNasc = 1989;
+let completedAge = anoAtual-anoNasc;
+const birthCity = 'São Bernardo do Campo';
 
-// let anoAtual = new Date ().getFullYear();
-// console.log(anoAtual);
+document.getElementById("a1").innerHTML ='<h3>Resposta:</h3>'
+document.getElementById("a1").innerHTML += '<p> Olá Mundo. Me chamo '+myName+'. Estou escrevendo esta frase em '+anoAtual+'. Portanto, no ano indicado estarei com '+completedAge+' anos completados. Eu nasci em '+birthCity+', no ano de '+ anoNasc+'.</p>';
 
-// let myName = 'Rafael';
-// console.log("Nome:"+ myName);
-
-// const birthCity = 'São Bernardo do Campo';
-// birthCity = 'São José dos Campos';
-// console.log('Cidade de Nascimento:' + birthCity);
-
-// let birthYear = 1989;
-// birthYear = 2030;
-// console.log('Ano de Nascimento:' + birthYear);
 
 // -----------------------------------------------------------------------
 
 // 2. JAVASCRIPT -TIPOS PRIMITIVOS, TIPAGEM DINÂMICA E OPERAÇÕES ARITMÉTICAS
 
 let movie = 'Avengers'; //string value;
+document.getElementById("a2").innerHTML ='<h3>Resposta:</h3>';
+document.getElementById("a2").innerHTML +=
+'<p>'+typeof movie+'</p>';
+
 let score = 10.89; //number value;
 let isValid = true; //boolean value;
 let name; //undefined
