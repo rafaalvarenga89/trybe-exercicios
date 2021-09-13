d0883a505cf78bb81d8ded354556c5f6c317bc92
Rafael de Alvarenga Reis <rafa_alvarenga@msn.com>
2021-09-13 20:14:29 -0300

    Commit 13092021-2014

---
+++ exercicios/Bloco-05/buscandoElementos/scriptBE.js
@@ -0,0 +1,44 @@
+let ingredientItens = [
+
+"500g de feijão carioquinha cozido",
+"200g de toucinho",
+"1 concha de óleo",
+"1 cebola média picada",
+"4 dentes de alho",
+"5 ovos",
+"1 colher de sopa de sal com alho",
+"Cheiro verde à gosto",
+"200g de farinha de mandioca",
+
+];
+
+let ingredientList = document.querySelector(".ingredient-list");
+
+for (let index = 0; index < ingredientItens.length; index += 1) {
+    let ingredient = ingredientItens[index];
+
+    let ingredientListItem = document.createElement('li');
+    ingredientListItem.innerText = ingredient;
+
+    ingredientList.appendChild(ingredientListItem);
+
+}
+
+let recipePreparationSteps = [
+
+"Coloque o óleo em uma panela e doure a cebola, acrescente o bacon e frite bem.",
+
+"Adicione o alho, sal e os ovos, misturando com cuidado para que não se despedacem muito.",
+
+"Retoque o feijão, baixe o fogo, misture a farinha aos poucos e o cheiro verde.",
+
+];
+
+let recipePreparation = document.querySelector(".recipe-preparation-section");
+
+for (let index2 = 0; index2 < recipePreparationSteps.length; index2 += 1) {
+    let steps = recipePreparationSteps[index2];
+    let recipeSteps = document.createElement('li');
+    recipeSteps.innerText = steps;
+    recipePreparation.appendChild(recipeSteps);
+}
