949a6d0777e37f7d93e26071ce603dbea34e25ae
Rafael de Alvarenga Reis <rafa_alvarenga@msn.com>
2021-09-14 14:37:53 -0300

    Commit 14092021-1437

--- exercicios/Bloco-05/fixacao5.2/script2.js
+++ exercicios/Bloco-05/fixacao5.2/script2.js
@@ -3,3 +3,25 @@ colorPai.style.color = 'orange';
 
 let text = document.getElementById('primeiroFilhoDoFilho');
 text.innerText = 'Não existe trabalho ruim. O ruim é ter que trabalhar'
+
+
+let pai = document.getElementById('pai');
+
+let quintoFilho = document.createElement('section');
+quintoFilho.id = 'quintoFilho';
+pai.appendChild(quintoFilho);
+
+
+let filho = document.getElementById('elementoOndeVoceEsta');
+let terceiroFilhoDoFilho = document.createElement('section');
+terceiroFilhoDoFilho.id = 'terceiroFilhoDoFilho';
+filho.appendChild(terceiroFilhoDoFilho);
+
+let filhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+let primeiroFilhoDoFilhoDoFilho = document.createElement('section');
+primeiroFilhoDoFilhoDoFilho.id = 'primeiroFilhoDoFilhoDoFilho';
+filhoDoFilho.appendChild(primeiroFilhoDoFilhoDoFilho);
+
+let filhoTres = document.getElementById('primeiroFilhoDoFilhoDoFilho').parentNode.parentNode.parentNode.children[2];
+
+console.log(filhoTres);
